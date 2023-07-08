23af584285dc025a1be56de3fcc6f52375fc94a6
Huseyin Kan <huseyin.kann@hotmail.com>
2023-07-08 19:38:03 +0300

    Slider

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,33 +1,17 @@
 import React from 'react'
+import Slider from './Parts/About/Slider'
 
 export default function About() {
+  
+
   return (
-    <div className=' -z-40 h-screen px-10 py-6 flex pt-16'>
-      <div className='basis-1/3 bg-gray-900 border-r-4 border-black rounded-l-md text-slate-300 px-8 transiton duration-300 py-4 shadow-md'>
-        Photo
+    <div className='-z-40 h-full px-10 py-6 flex md:flex-row flex-col pt-16'>
+      <div className='md:imgMd md:centered bg-gray-800 centered '>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIynxwpEPFhGpU7q1R_FGuH7Kj__INOSQjhRptUaco5unvKjzT7S3ogOqASmM-UMEYugk&usqp=CAU' alt='Profile Pic' className='md:rounded-md  rounded-full h-20 w-20 m-4   shadow-md md:w-full md:h-full object-contain'></img>
       </div>
-      <div className='flex-col centered basis-2/3 bg-gray-900 rounded-r-md text-slate-300 shadow-md'>
-        <div className='flex  justify-around items-center w-full'>
-        <button className='swapButton centered tde'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-          </svg>
-        </button>
-        <div className='flex flex-col w-2/3 centered gap-6 rounded-md shadow-md bg-gray-700/30 px-4 py-4'>
-          <h1 className='font-bold text-xl'>Welcome</h1>
-          <div className='text-center'>
-            First of all, welcome to the person that visits my website.
-             I made this site because of work opportunities,
-              but if there are any suggestions to improve myself or this website,
-               you can contact me (and I will appreciate it).
-          </div>
-          <h6 className='text-sm text-slate-600 italic'>Open to work</h6>
-        </div>
-        <button className='swapButton centered tde'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </button>
+      <div className='flex-col centered rounded-md grow md:basis-2/3 bg-gray-800 md:rounded-r-md text-slate-300 shadow-md'>
+        <div className='flex justify-around items-center w-full overflow-hidden'>
+        <Slider/>
         </div>
       </div>
     </div>
---
+++ src/datas/textData.js
@@ -0,0 +1,22 @@
+export const textData = [
+    {
+        header:"Welcome",
+        main:"First of all, welcome to the person that visits my website. I made this site because of work opportunities, but if there are any suggestions to improve myself or this website, you can contact me (and I will appreciate it)."
+    },
+    {
+        header:"About me",
+        main:"I'm a fresh graduated computer engineer. I started to web development two years ago. I usually work on frontend but there are also fullstack projects made by me. I want to improve myself with working and I'm really hungry for that."
+    },
+    {
+        header:"Skills",
+        main: 'React,Javascript,Git,Node,.Net,HTML,CSS'
+    },
+    {
+        header:'Education',
+        main:'Mersin University Computer Engineer. 2019-2023'
+    },
+    {
+        header:'Work Exeperience',
+        main:'Adana Büyükşehir Belediyesi Yazılım Birimi. 27.06.2022-01.09.2022'
+    }
+]
